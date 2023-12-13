d6a19ce16fb513066061be3ec1fa12a9e1891dd1
ivajunu <ivanna.junuzovic@live.se>
2023-12-13 23:02:15 +0100

    front page wip

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
 import "./App.css";
 import HomePage from "./components/HomePage";
 import styled from "styled-components";
+import MenuIcon from "@mui/icons-material/Menu";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+
+// källa: https://mui.com/material-ui/react-menu/
 
 import {
   createHashRouter,
@@ -8,30 +14,32 @@ import {
   Outlet,
   RouterProvider,
 } from "react-router-dom";
+import { useState } from "react";
 
 const StyledNav = styled.nav`
   display: flex;
-  align-items: center;
+  justify-content: space-between;
   background-color: #f3a3ca;
   padding: 10px;
   border-bottom: 0.5px solid #e591b9;
   border-bottom-left-radius: 50%;
-  border-bottom-right-radius: 20%;
+  /* border-bottom-right-radius: 20%; */
   box-shadow: 0 3px 10px rgb(164 157 146 / 0.2);
 `;
 
-const StyledUl = styled.ul`
-  display: flex;
-  justify-content: space-evenly;
-  width: 100%;
-  list-style-type: none;
-  padding: 0;
-  margin: 0;
-`;
+// const StyledUl = styled.ul`
+//   display: flex;
+//   justify-content: space-evenly;
+//   width: 100%;
+//   list-style-type: none;
+//   padding: 0;
+//   margin: 0;
+// `;
 
 const StyledLinks = styled(Link)`
   text-decoration: none;
   font-size: 20px;
+  font-family: "Dosis", sans-serif;
   font-weight: 800;
   color: #5c1337;
   padding: 0;
@@ -46,6 +54,19 @@ const FlowerIcon = styled.img`
   z-index: 2;
   padding: 0;
   margin: 0;
+`;
+
+const StyledMenuItem = styled(MenuItem)`
+  && {
+    background-color: #f3a3ca;
+    padding: 10px;
+    outline: #5c1337 solid 1px;
+  }
+`;
+
+const StyledH1 = styled.h1`
+  font-size: 38px;
+  color: #5c1337;
 `;
 
 export default function App() {
@@ -64,21 +85,62 @@ export default function App() {
 }
 
 function Main() {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <>
       <StyledNav>
         <FlowerIcon src="../public/flowericon.png" />
-        <StyledUl>
-          <li>
-            <StyledLinks to="/">HOME</StyledLinks>
-          </li>
-          <li>
-            <StyledLinks to="/about">ABOUT</StyledLinks>
-          </li>
-          <li>
-            <StyledLinks to="/contact">CONTACT</StyledLinks>
-          </li>
-        </StyledUl>
+        <StyledH1>Harmony Run</StyledH1>
+        <div>
+          <Button
+            id="basic-button"
+            aria-controls={open ? "basic-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            onClick={handleClick}
+          >
+            <MenuIcon
+              sx={{
+                color: "black",
+                width: "50px",
+                height: "45px",
+              }}
+            />
+          </Button>
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              "aria-labelledby": "basic-button",
+            }}
+            sx={{
+              "& .MuiMenu-list": {
+                padding: "0",
+                margin: "0",
+              },
+            }}
+          >
+            <StyledMenuItem>
+              <StyledLinks to="/">HOME</StyledLinks>
+            </StyledMenuItem>
+            <StyledMenuItem>
+              <StyledLinks to="/about">ABOUT</StyledLinks>
+            </StyledMenuItem>
+            <StyledMenuItem>
+              <StyledLinks to="/contact">CONTACT</StyledLinks>
+            </StyledMenuItem>
+          </Menu>
+        </div>
       </StyledNav>
       <main>
         <Outlet />
