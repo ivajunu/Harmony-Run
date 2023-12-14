a8c5adfc4701140271122d54d2828a46c90712d0
ivajunu <ivanna.junuzovic@live.se>
2023-12-14 21:44:58 +0100

    Added some styling and installed cypress

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,80 +1,28 @@
+// källa: https://mui.com/material-ui/react-menu/
+
 import "./App.css";
-import HomePage from "./components/HomePage";
-import styled from "styled-components";
+import HomePage from "./components/HomePage/HomePage";
 import MenuIcon from "@mui/icons-material/Menu";
 import Button from "@mui/material/Button";
 import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-
-// källa: https://mui.com/material-ui/react-menu/
-
-import {
-  createHashRouter,
-  Link,
-  Outlet,
-  RouterProvider,
-} from "react-router-dom";
+import { createHashRouter, Outlet, RouterProvider } from "react-router-dom";
 import { useState } from "react";
+import {
+  FlowerIcon,
+  StyledH1,
+  StyledLinks,
+  StyledMenuItem,
+  StyledNav,
+} from "./App.styled";
 
-const StyledNav = styled.nav`
-  display: flex;
-  justify-content: space-between;
-  background-color: #f3a3ca;
-  padding: 10px;
-  border-bottom: 0.5px solid #e591b9;
-  border-bottom-left-radius: 50%;
-  /* border-bottom-right-radius: 20%; */
-  box-shadow: 0 3px 10px rgb(164 157 146 / 0.2);
-`;
-
-// const StyledUl = styled.ul`
-//   display: flex;
-//   justify-content: space-evenly;
-//   width: 100%;
-//   list-style-type: none;
-//   padding: 0;
-//   margin: 0;
-// `;
-
-const StyledLinks = styled(Link)`
-  text-decoration: none;
-  font-size: 20px;
-  font-family: "Dosis", sans-serif;
-  font-weight: 800;
-  color: #5c1337;
-  padding: 0;
-  margin: 0;
-  &:focus {
-    text-shadow: 1px 1px 2px lightgrey, 0 0 1em lightgrey, 0 0 0.2em lightgrey;
-  }
-`;
-
-const FlowerIcon = styled.img`
-  height: 80px;
-  z-index: 2;
-  padding: 0;
-  margin: 0;
-`;
-
-const StyledMenuItem = styled(MenuItem)`
-  && {
-    background-color: #f3a3ca;
-    padding: 10px;
-    outline: #5c1337 solid 1px;
-  }
-`;
-
-const StyledH1 = styled.h1`
-  font-size: 38px;
-  color: #5c1337;
-`;
+import DailyHealth from "./components/Health/DailyHealth";
 
 export default function App() {
   const router = createHashRouter([
     {
       children: [
         { element: <HomePage />, path: "/" },
-        // { element: <About />, path: "/about" },
+        { element: <DailyHealth />, path: "/daily-health" },
         // { element: <Contact />, path: "/contact" },
       ],
       element: <Main />,
@@ -109,7 +57,7 @@ function Main() {
           >
             <MenuIcon
               sx={{
-                color: "black",
+                color: "#5c1337",
                 width: "50px",
                 height: "45px",
               }}
@@ -131,13 +79,13 @@ function Main() {
             }}
           >
             <StyledMenuItem>
-              <StyledLinks to="/">HOME</StyledLinks>
+              <StyledLinks to="/">Home</StyledLinks>
             </StyledMenuItem>
             <StyledMenuItem>
-              <StyledLinks to="/about">ABOUT</StyledLinks>
+              <StyledLinks to="/daily-health">Daily Health</StyledLinks>
             </StyledMenuItem>
             <StyledMenuItem>
-              <StyledLinks to="/contact">CONTACT</StyledLinks>
+              <StyledLinks to="/contact">Contact</StyledLinks>
             </StyledMenuItem>
           </Menu>
         </div>
---
+++ frontend/src/components/HomePage/HomePage.styled.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const StyledContainer = styled.div`
+  padding: 10px;
+`;
+
+export const StyledDiv = styled.div`
+  border: solid #e591b9 1px;
+  border-radius: 10px;
+  padding: 10px;
+  background-color: #f3a3ca;
+  box-shadow: 0 3px 10px rgb(164 157 146 / 0.2);
+`;
