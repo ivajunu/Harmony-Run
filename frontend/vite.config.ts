8a8c5e0c99ca6625f2758a69ab107f7c34e1d4e8
ivajunu <ivanna.junuzovic@live.se>
2023-12-14 22:21:42 +0100

    Added config file

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dns from "dns"
+dns.setDefaultResultOrder("ipv4first")
 
 // https://vitejs.dev/config/
 export default defineConfig({
